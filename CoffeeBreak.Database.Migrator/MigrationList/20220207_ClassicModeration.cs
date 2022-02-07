65d78b1595363674bdb834f51427bbe7282a1824
Ikramullah <ikramullahzx@gmail.com>
2022-02-07 13:05:29 +0800

    add: Migrator

---
+++ CoffeeBreak.Database.Migrator/MigrationList/20220207_ClassicModeration.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace CoffeeBreak.Database.Migrator.MigrationList;
+[Migration(20220207)]
+public class _20220207_ClassicModeration : Migration
+{
+    public override void Up()
+    {
+        this.Create.Table("WarnList")
+            .WithColumn("ID").AsInt64().PrimaryKey().Identity().NotNullable()
+            .WithColumn("GuildID").AsInt64().NotNullable()
+            .WithColumn("UserID").AsInt64().NotNullable()
+            .WithColumn("ExecutorID").AsInt64().NotNullable()
+            .WithColumn("Reason").AsString().NotNullable()
+            .WithColumn("Timestamp").AsDateTime().WithDefault(SystemMethods.CurrentDateTime);
+    }
+
+    public override void Down()
+    {
+        this.Delete.Table("WarnList");
+    }
+}
---
+++ CoffeeBreak.Database.Migrator/Program.cs
@@ -0,0 +1,41 @@
+﻿using CoffeeBreak.Database.Migrator.MigrationList;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoffeeBreak.Database.Migrator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceProvider = CreateServices();
+        using var scope = serviceProvider.CreateScope();
+        var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+        switch (args[0])
+        {
+            case "--up": runner.MigrateUp(); break;
+            case "--down": runner.MigrateDown(long.Parse(args[1] ?? "0")); break;
+        }
+    }
+
+    private static IServiceProvider CreateServices()
+    {
+        return new ServiceCollection()
+            .AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb.AddMySql5().WithGlobalConnectionString(GenerateConnectionString())
+                    // Add migration list here
+                    .ScanIn(typeof(_20220207_ClassicModeration).Assembly).For.Migrations()
+                )
+            .AddLogging(lb => lb.AddFluentMigratorConsole())
+            .BuildServiceProvider(false);
+    }
+
+    private static string GenerateConnectionString()
+    {
+        string server = Environment.GetEnvironmentVariable("MARIADB_HOST") ?? "localhost";
+        string database = Environment.GetEnvironmentVariable("MARIADB_DATABASE") ?? "coffeebreak";
+        string username = Environment.GetEnvironmentVariable("MARIADB_USERNAME") ?? "root";
+        string password = Environment.GetEnvironmentVariable("MARIADB_PASSWORD") ?? "";
+        return $"server={server};database={database};user={username};password={password}";
+    }
+}
