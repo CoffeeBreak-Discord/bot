2dd0a7464bf9f2314b5809e53f33f521a5c9d871
Ikramullah <ikramullahzx@gmail.com>
2022-02-16 13:29:43 +0800

    add: permission checking for moderation/kick

--- CoffeeBreak/Modules/Moderation/Kick.cs
+++ CoffeeBreak/Modules/Moderation/Kick.cs
@@ -7,6 +7,32 @@ public partial class ModerationModule : InteractionModuleBase<ShardedInteraction
     [SlashCommand("kick", "Kick user")]
     public async Task Kick([Summary(description: "The person who want to kicked")] IUser user)
     {
-        await this.RespondAsync($"You'll kick {user}");
+        // Check if command is executed in guild
+        if (this.Context.Guild.Equals(null))
+        {
+            await this.RespondAsync("You can only using this module in Guild/Server");
+            return;
+        }
+
+        var guildUser = this.Context.Guild.GetUser(user.Id);
+        
+        // Check if user is urself, owner, admin, or have role higher than executor/bot.
+        if (this.Context.User.Id == user.Id)
+        {
+            await this.RespondAsync("You cannot kick yourself");
+            return;
+        }
+        if (this.Context.Guild.OwnerId == guildUser.Id)
+        {
+            await this.RespondAsync("You cannot kick an owner");
+            return;
+        }
+        if (guildUser.Roles.Where(x => x.Permissions.Has(GuildPermission.Administrator)).Count() > 0)
+        {
+            await this.RespondAsync("You cannot kick an admin");
+            return;
+        }
+
+        await this.RespondAsync($"You'll kick {guildUser}");
     }
 }
