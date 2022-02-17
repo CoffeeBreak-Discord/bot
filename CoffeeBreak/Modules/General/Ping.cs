ec7330ed566fcb879312bda4a2d932a7deb0b56b
Hazmi35 <contact@hzmi.xyz>
2022-02-17 21:19:13 +0700

    feat(Ping): Better ping command. now show bot response time in ms (#10)

--- CoffeeBreak/Modules/General/Ping.cs
+++ CoffeeBreak/Modules/General/Ping.cs
@@ -1,11 +1,16 @@
+using Discord;
 using Discord.Interactions;
 
 namespace CoffeeBreak.Modules;
 public partial class GeneralModule : InteractionModuleBase<ShardedInteractionContext>
 {
-    [SlashCommand("ping", "Ping!")]
+    [SlashCommand("ping", "Ping! - Show the ping / response time of the bot in ms")]
     public async Task Ping()
     {
-        await this.RespondAsync("Pong!");
+        await this.RespondAsync("🏓 | Pong!");
+        IUserMessage Message = await this.GetOriginalResponseAsync();
+        long TimeNow = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        long botPing = TimeNow - Message.Timestamp.ToUnixTimeMilliseconds();
+        await this.ModifyOriginalResponseAsync(Message => Message.Content = $"🏓 | Pong! - **{botPing}ms**");
     }
 }
