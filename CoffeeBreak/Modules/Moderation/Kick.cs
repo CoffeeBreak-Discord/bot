d758a83c71c4698124252ea070bf7559cd4280bb
Ikramullah <ikramullahzx@gmail.com>
2022-02-18 00:55:24 +0800

    add: Filter for kick module

--- CoffeeBreak/Modules/Moderation/Kick.cs
+++ CoffeeBreak/Modules/Moderation/Kick.cs
@@ -1,5 +1,6 @@
 using Discord;
 using Discord.Interactions;
+using Discord.WebSocket;
 
 namespace CoffeeBreak.Modules;
 public partial class ModerationModule : InteractionModuleBase<ShardedInteractionContext>
@@ -7,34 +8,50 @@ public partial class ModerationModule : InteractionModuleBase<ShardedInteraction
     [SlashCommand("kick", "Kick user")]
     public async Task Kick([Summary(description: "The person who want to kicked")] IUser user)
     {
+        SocketGuildUser? guildUser = this.Context.Guild.GetUser(user.Id);
+        SocketGuildUser ctxUser = this.Context.Guild.GetUser(this.Context.User.Id);
+        SocketGuildUser clientUser = this.Context.Guild.GetUser(_client.CurrentUser.Id);
+
         // Check if command is executed in guild
         if (this.Context.Guild == null)
         {
-            await this.RespondAsync("You can only using this module in Guild/Server");
+            await this.RespondAsync("You can only using this module in Guild/Server.");
             return;
         }
 
-        var guildUser = this.Context.Guild.GetUser(user.Id);
-        
+        // Check if user didn't found
+        if (guildUser == null)
+        {
+            await this.RespondAsync("User not found!");
+            return;
+        }
+
         // Check if user is urself, owner, admin, or have role higher than executor/bot.
         if (this.Context.User.Id == user.Id)
         {
-            await this.RespondAsync("You cannot kick yourself");
+            await this.RespondAsync("You cannot kick yourself.");
             return;
         }
         if (this.Context.Guild.OwnerId == guildUser.Id)
         {
-            await this.RespondAsync("You cannot kick an owner");
+            await this.RespondAsync("You cannot kick an owner.");
             return;
         }
         if (guildUser.Roles.Where(x => x.Permissions.Has(GuildPermission.Administrator)).Count() > 0)
         {
-            await this.RespondAsync("You cannot kick an admin");
+            await this.RespondAsync("You cannot kick an admin.");
             return;
         }
-
-        var botRole = this.Context.Guild.GetUser(_client.CurrentUser.Id) .Roles.OrderByDescending(x => x.Position);
-        var botPosition = botRole.Count() > 0 ? botRole.ToArray()[0].Position : -1;
+        if (!this.IsExecutable(ctxUser, guildUser))
+        {
+            await this.RespondAsync("You cannot kick user that higher than you.");
+            return;
+        }
+        if (!this.IsExecutable(ctxUser, clientUser))
+        {
+            await this.RespondAsync("You cannot kick user that higher from this bot.");
+            return;
+        }
 
         await this.RespondAsync($"You'll kick {guildUser}");
     }
