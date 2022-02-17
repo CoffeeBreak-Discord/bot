1ec24f80eabb19776dd5d5115b88a44a96a3e50a
Hazmi35 <contact@hzmi.xyz>
2022-02-17 22:44:48 +0700

    feat(Version): A Command to print bot version (#11)

---
+++ CoffeeBreak/Modules/General/Version.cs
@@ -0,0 +1,14 @@
+using Discord;
+using Discord.Interactions;
+
+namespace CoffeeBreak.Modules;
+public partial class GeneralModule : InteractionModuleBase<ShardedInteractionContext>
+{
+    [SlashCommand("version", "Print the bot version")]
+    public async Task Version()
+    {
+        string Version = System.Environment.GetEnvironmentVariable("VERSION") ?? "Development";
+        string CommitSHA = System.Environment.GetEnvironmentVariable("COMMIT") ?? "Unknown";
+        await this.RespondAsync($"Version: **{Version}**\nCommit SHA: **{CommitSHA}**");
+    }
+}
