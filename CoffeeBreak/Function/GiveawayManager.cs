47807478d68e8a88d17444375f42be12f05161de
Hazmi35 <contact@hzmi.xyz>
2022-03-15 15:31:43 +0000

    fix(GiveawayManager): Fix EndDate value

--- CoffeeBreak/Function/GiveawayManager.cs
+++ CoffeeBreak/Function/GiveawayManager.cs
@@ -50,7 +50,7 @@ public class GiveawayManager
             .AddField("Note", data.GiveawayNote)
             .AddField("Creator", $"<@!{data.UserID}>", true)
             .AddField("Role", parseRole.Count() == 0 ? "No minimum/required role." : string.Join("\n", parseRole.ToArray()), true)
-            .AddField("End Date", $"${endTime.longDateTime()} (${endTime.relative()})")
+            .AddField("End Date", $"{endTime.longDateTime()} ({endTime.relative()})")
             .AddField("Entries/Winner", $"0 people / {data.WinnerCount} winner", true);
         return embed.Build();
     }
