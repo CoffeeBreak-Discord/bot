e399e9930c02f61d9bbd6cf33b8d36fb0eb99426
Ikramullah <ikramullahzx@gmail.com>
2022-03-28 21:17:09 +0800

    add(stage): join & leave from stage

--- CoffeeBreak/Modules/VoiceManager/Stage.cs
+++ CoffeeBreak/Modules/VoiceManager/Stage.cs
@@ -1,3 +1,4 @@
+using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
 using StackExchange.Redis;
@@ -13,5 +14,49 @@ public partial class VoiceManagerStageModule : InteractionModuleBase<ShardedInte
     {
         _client = client;
         _cache = cache;
+    }
+
+    // TODO: Make command
+    // /stage join
+    // /stage leave
+    // /stage roles
+    [RequireUserPermission(GuildPermission.ManageGuild)]
+    [RequireUserPermission(GuildPermission.ManageEvents)]
+    [RequireBotPermission(GuildPermission.MoveMembers)]
+    [SlashCommand("join", "Make the bot watching the stage.", runMode: RunMode.Async)]
+    public async Task JoinCommandAsync(
+        [ChannelTypes(ChannelType.Stage), Summary(description: "Stage target")] IStageChannel? channel = null)
+    {
+        var userState = this.Context.User as IVoiceState;
+        if (userState == null) return;
+        channel = channel == null ? userState.VoiceChannel as IStageChannel : channel;
+
+        if (channel == null)
+        {
+            await this.RespondAsync(
+                "You didn't joined the stage. Please join again.",
+                ephemeral: true);
+            return;
+        }
+
+        await channel.ConnectAsync();
+        await this.RespondAsync($"Joined **{channel.Name}** stage.", ephemeral: true);
+    }
+
+    [RequireUserPermission(GuildPermission.ManageGuild)]
+    [RequireUserPermission(GuildPermission.ManageEvents)]
+    [RequireBotPermission(GuildPermission.MoveMembers)]
+    [SlashCommand("leave", "Make the bot watching the stage.", runMode: RunMode.Async)]
+    public async Task LeaveCommandAsync()
+    {
+        var channel = this.Context.Guild.GetUser(_client.CurrentUser.Id).VoiceChannel as IStageChannel;
+        if (channel == null)
+        {
+            await this.RespondAsync("This bot didn't joined any stage.", ephemeral: true);
+            return;
+        }
+
+        await channel.DisconnectAsync();
+        await this.RespondAsync($"Bot successfully disconnected from **{channel.Name}** stage.", ephemeral: true);
     }
 }
---
+++ CoffeeBreak/Modules/VoiceManager/StageResponse.cs
@@ -0,0 +1,5 @@
+namespace CoffeeBreak.Modules;
+public partial class VoiceManagerStageModule
+{
+    //
+}
