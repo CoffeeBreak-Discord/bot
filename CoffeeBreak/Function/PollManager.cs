914f0bf72e92e51f2a00c0f0d72ab2055880d84f
Ikramullah <ikramullahzx@gmail.com>
2022-04-26 22:10:08 +0800

    refactor(poll): remove poll, because dunno why the object is the undefined

--- CoffeeBreak/Function/PollManager.cs
+++ CoffeeBreak/Function/PollManager.cs
@@ -4,33 +4,21 @@ using Discord.Interactions;
 namespace CoffeeBreak.Function;
 public class PollManager
 {
-    public class PollTemplateModal
+    public class SingleChoiceModal : IModal
     {
+        public string Title => "Make a single poll choice!";
+
         [InputLabel("Insert your poll name")]
         [ModalTextInput("poll_name", placeholder: "Ex: What's your favorite fruit?")]
-        public string PollName { get; set; } = default!;
+        public string Name { get; set; } = default!;
 
-        [InputLabel("How long this poll?")]
+        [InputLabel("How long this giveaway?")]
         [ModalTextInput("poll_duration", placeholder: "Ex: 3d 4h 5m 10s")]
         public string Duration { get; set; } = default!;
 
-        [InputLabel("Any choice?")]
-        [ModalTextInput( "poll_choice", TextInputStyle.Paragraph, "Separate the choice with new line.", 1, 255)]
+        [InputLabel("Some note?")]
+        [ModalTextInput("poll_choice", TextInputStyle.Paragraph, "Separate the choice with new line", 1, 255)]
         public string Choice { get; set; } = default!;
-    }
-
-    public class PollSingleChoiceModal : PollTemplateModal, IModal
-    {
-        public string Title => "Make a single poll choice!";
-    }
-
-    public class PollMultipleChoiceModal : PollTemplateModal, IModal
-    {
-        public string Title => "Make a multiple poll choice!";
-
-        [InputLabel("How much user can choose.")]
-        [ModalTextInput("poll_count_choice", placeholder: "Ex: 3d 4h 5m 10s")]
-        public string CountChoice { get; set; } = default!;
     }
 
     public enum PollChoiceType
@@ -46,7 +34,7 @@ public class PollManager
             .WithPlaceholder("Select an option.")
             .WithCustomId($"menu_poll:{id}")
             .WithMinValues(1);
-        
+
         if (type == PollChoiceType.Multiple) builder.WithMaxValues(count);
         else builder.WithMaxValues(1);
 
