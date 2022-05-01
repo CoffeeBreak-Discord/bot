b56ac33b1d5574bc727b48b6fe25374c26e29db9
Ikramullah <ikramullahzx@gmail.com>
2022-05-01 18:03:16 +0800

    fix(poll): isoptionalsrequired set default to true

---
+++ CoffeeBreak.Migrator/MigrationList/20220501_PollHotfix.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace CoffeeBreak.Migrator.MigrationList;
+[Migration(20220501)]
+public class _20220501_PollHotfix : Migration
+{
+    public override void Up()
+    {
+        this.Alter.Table("PollRunning")
+            .AlterColumn("IsOptionsRequired").AsBoolean().NotNullable().WithDefaultValue(true);
+    }
+
+    public override void Down()
+    {
+        this.Alter.Table("PollRunning")
+            .AlterColumn("IsOptionsRequired").AsBoolean().NotNullable().WithDefaultValue(false);
+    }
+}
--- CoffeeBreak.Migrator/Program.cs
+++ CoffeeBreak.Migrator/Program.cs
@@ -30,6 +30,7 @@ class Program
                     .ScanIn(typeof(_20220311_GiveawayParticipant).Assembly).For.Migrations()
                     .ScanIn(typeof(_20220328_StageConfig).Assembly).For.Migrations()
                     .ScanIn(typeof(_20220422_Poll).Assembly).For.Migrations()
+                    .ScanIn(typeof(_20220501_PollHotfix).Assembly).For.Migrations()
                 )
             .AddLogging(lb => lb.AddFluentMigratorConsole())
             .BuildServiceProvider(false);
